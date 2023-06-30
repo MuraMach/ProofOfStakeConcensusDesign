1d7dbf07cf35360d5f2f7e28da589dc79aee0873
Leos <136538188+MuraMach@users.noreply.github.com>
2023-06-30 18:45:36 -0400

    Create MasterNode.py

---
+++ Usable_Tests/Python_0_003/MasterNode.py
@@ -0,0 +1,10 @@
+from Player import Player
+
+class MasterNode(Player):
+    def __init__(self, name, blockchain):
+        super().__init__(name)
+        self.blockchain = blockchain
+    
+    def make_move(self, board):
+        print(f'{self.name} is making a move.')
+        return self.blockchain.solve_block()
